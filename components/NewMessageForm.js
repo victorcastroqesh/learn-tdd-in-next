8c410296b48af1f165a1fedbf334e00e6b299a10
Victor Castro <victor.castro@qesh.ai>
2022-10-24 15:06:28 -0300

    :sparkles: criei component: NewMessageForm e seu teste unitário

--- components/NewMessageForm.js
+++ components/NewMessageForm.js
@@ -1,8 +1,27 @@
-export default function NewMessageForm() {
+import { useState } from "react";
+
+export default function NewMessageForm({ onSend }) {
+  const [inputText, setInputText] = useState("");
+
+  function handleTextChange(event) {
+    setInputText(event.target.value);
+  }
+  function handleSend() {
+    onSend(inputText);
+    setInputText("");
+  }
+
   return (
     <>
-      <input type="text" data-testid="messageText" />
-      <button data-testid="sendButton">Send</button>
+      <input
+        type="text"
+        data-testid="messageText"
+        value={inputText}
+        onChange={handleTextChange}
+      />
+      <button data-testid="sendButton" onClick={handleSend}>
+        Send
+      </button>
     </>
   );
 }
